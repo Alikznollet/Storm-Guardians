102635f8188c5a9a695f32acd16b915f8d9af59a
Alexander Nollet <alexanderacj13@gmail.com>
2024-09-11 21:46:14 +0200

    skip button and shadows
    + font shadows for every piece of text
    + skip button so the player can manually skip the hour before the storm

--- scenes/ui/hud.gd
+++ scenes/ui/hud.gd
@@ -13,7 +13,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	%WaveLable.text = GameState.current_wave
-	%TimerLable.text = "%.2f" % GameState.grace_timer.get_time_left()
+	%TimerLable.text = "%.f" % GameState.grace_timer.get_time_left()
 	
 	if GameState.current_balance < int(%SpearPrice.text):
 		%SpearPrice.modulate = Color.RED
@@ -45,6 +45,7 @@ func _grace_hide() -> void:
 	_handle_fire(false)
 	_handle_bomb(false)
 	_handle_mage(false)
+	$Timer/Timer/TimerButton.button_pressed = false
 
 func _on_wave_manager_wave_over() -> void:
 	_grace_show()
@@ -60,41 +61,41 @@ func _on_spear_mouse_exited() -> void:
 func _handle_spear(open: bool) -> void:
 	if open:
 		var tween: Tween = get_tree().create_tween()
-		tween.tween_property($Buy/Prices/PriceSpear, "position", Vector2(0, 0), 0.5).set_trans(Tween.TRANS_CUBIC)
+		tween.tween_property($Buy/Prices/PriceSpear, "position", Vector2(0, 0), 0.3).set_trans(Tween.TRANS_CUBIC)
 		tween.play()
 	else:
 		var tween: Tween = get_tree().create_tween()
-		tween.tween_property($Buy/Prices/PriceSpear, "position", Vector2(0, 16), 0.5).set_trans(Tween.TRANS_CUBIC)
+		tween.tween_property($Buy/Prices/PriceSpear, "position", Vector2(0, 16), 0.3).set_trans(Tween.TRANS_CUBIC)
 		tween.play()
 
 func _handle_bomb(open: bool) -> void:
 	if open:
 		var tween: Tween = get_tree().create_tween()
-		tween.tween_property($Buy/Prices/PriceBomb, "position", Vector2(0, 0), 0.5).set_trans(Tween.TRANS_CUBIC)
+		tween.tween_property($Buy/Prices/PriceBomb, "position", Vector2(0, 0), 0.3).set_trans(Tween.TRANS_CUBIC)
 		tween.play()
 	else:
 		var tween: Tween = get_tree().create_tween()
-		tween.tween_property($Buy/Prices/PriceBomb, "position", Vector2(0, 16), 0.5).set_trans(Tween.TRANS_CUBIC)
+		tween.tween_property($Buy/Prices/PriceBomb, "position", Vector2(0, 16), 0.3).set_trans(Tween.TRANS_CUBIC)
 		tween.play()
 
 func _handle_fire(open: bool) -> void:
 	if open:
 		var tween: Tween = get_tree().create_tween()
-		tween.tween_property($Buy/Prices/PriceFire, "position", Vector2(0, 0), 0.5).set_trans(Tween.TRANS_CUBIC)
+		tween.tween_property($Buy/Prices/PriceFire, "position", Vector2(0, 0), 0.3).set_trans(Tween.TRANS_CUBIC)
 		tween.play()
 	else:
 		var tween: Tween = get_tree().create_tween()
-		tween.tween_property($Buy/Prices/PriceFire, "position", Vector2(0, 16), 0.5).set_trans(Tween.TRANS_CUBIC)
+		tween.tween_property($Buy/Prices/PriceFire, "position", Vector2(0, 16), 0.3).set_trans(Tween.TRANS_CUBIC)
 		tween.play()
 
 func _handle_mage(open: bool) -> void:
 	if open:
 		var tween: Tween = get_tree().create_tween()
-		tween.tween_property($Buy/Prices/PriceMage, "position", Vector2(0, 0), 0.5).set_trans(Tween.TRANS_CUBIC)
+		tween.tween_property($Buy/Prices/PriceMage, "position", Vector2(0, 0), 0.3).set_trans(Tween.TRANS_CUBIC)
 		tween.play()
 	else:
 		var tween: Tween = get_tree().create_tween()
-		tween.tween_property($Buy/Prices/PriceMage, "position", Vector2(0, 16), 0.5).set_trans(Tween.TRANS_CUBIC)
+		tween.tween_property($Buy/Prices/PriceMage, "position", Vector2(0, 16), 0.3).set_trans(Tween.TRANS_CUBIC)
 		tween.play()
 
 func _on_bomb_mouse_entered() -> void:
@@ -120,3 +121,15 @@ func _on_electric_mouse_entered() -> void:
 func _on_electric_mouse_exited() -> void:
 	if GameState.grace_period:
 		_handle_mage(false)
+
+func _on_timer_button_toggled(toggled_on: bool) -> void:
+	var tween: Tween = get_tree().create_tween()
+	if toggled_on:
+		tween.tween_property($Timer/Confirm, "position", Vector2(0, 0), 0.3).set_trans(Tween.TRANS_CUBIC)
+	else:
+		tween.tween_property($Timer/Confirm, "position", Vector2(0, 16), 0.3).set_trans(Tween.TRANS_CUBIC)
+	tween.play()
+	
+func _on_skip_button_pressed() -> void:
+	GameState.grace_timer.stop()
+	GameState.grace_timer.timeout.emit()
