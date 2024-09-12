9aee0c2ad2ffdb499202ebed5a4a9b8d4e86faa1
Alexander Nollet <alexanderacj13@gmail.com>
2024-09-12 22:01:40 +0200

    tutorial
    + finished tutorial
    + gave Bert lore

--- scenes/tutorial/yapper.gd
+++ scenes/tutorial/yapper.gd
@@ -1,5 +1,13 @@
 extends Sprite2D
 
+func _ready() -> void:
+	$Welcome.hide()
+	$Money.hide()
+	$Interest.hide()
+	$Timer.hide()
+	$Towers.hide()
+	$Health.hide()
+	$End.hide()
 
 func hand_up():
 	$YapperHand.rotation_degrees = -90
@@ -9,3 +17,30 @@ func hand_down():
 	
 func hand_normal():
 	$YapperHand.rotation_degrees = 0
+	
+func show_welcome():
+	$Welcome.show()
+	
+func show_money():
+	$Welcome.hide()
+	$Money.show()
+	
+func show_interest():
+	$Money.hide()
+	$Interest.show()
+	
+func show_timer():
+	$Interest.hide()
+	$Timer.show()
+
+func show_towers():
+	$Timer.hide()
+	$Towers.show()
+	
+func show_health():
+	$Towers.hide()
+	$Health.show()
+	
+func show_end():
+	$Health.hide()
+	$End.show()
